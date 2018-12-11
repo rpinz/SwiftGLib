8fbdc9b908ed1920585fd7120ee23077a65c86f1
Ron Pinz <ron.pinz@gmail.com>
2018-12-11 17:15:30 -0500

    update swift 5 manifest update 4.2 manifest update CGLib submodule

--- Package@swift-5.swift
+++ Package@swift-5.swift
@@ -2,32 +2,51 @@
 
 import PackageDescription
 
-let package = Package(
+internal let package = Package(
     name: "GLib",
     products: [
         .library(
             name: "GLib",
-            targets: ["GLib"])
+            targets: [
+                "GLib"
+            ]
+        )
     ],
-    dependencies: [],
+    dependencies: [
+    ],
     targets: [
         .target(
             name: "GLib",
-            dependencies: ["CGLib"],
-            path: "Sources"),
+            dependencies: [
+                "CGLib"
+            ],
+            path: "Sources"
+        ),
         .testTarget(
             name: "GLibTests",
-            dependencies: ["GLib"]),
+            dependencies: [
+                "GLib"
+            ]
+        ),
         .systemLibrary(
             name: "CGLib",
             path: "Library/CGLib",
             pkgConfig: "glib-2.0 gio-unix-2.0",
             providers: [
-                .brew(["glib glib-networking gobject-introspection"]),
-                .apt(["libglib2.0-dev glib-networking gobject-introspection libgirepository1.0-dev"])
-            ])
+                .brew(
+                    [
+                        "glib glib-networking gobject-introspection"
+                    ]
+                ),
+                .apt(
+                    [
+                        "libglib2.0-dev glib-networking gobject-introspection libgirepository1.0-dev"
+                    ]
+                )
+            ]
+        )
     ],
     swiftLanguageVersions: [
-        .v4_2
+        .v5
     ]
 )
